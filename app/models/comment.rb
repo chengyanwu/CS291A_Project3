4cc4a84f9734d12d34ab3f57eedb569713ab2b53
chengyanwu <cheng-yan@umail.ucsb.edu>
2022-10-18 20:13:06 -0700

    add blank validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
     belongs_to :post
     validates :comment, length: { maximum: 140 }
+    validates_presence_of :comment
+    validates_presence_of :user_id
+    validates_presence_of :post_id
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
     validates :content, length: { maximum: 140 }
+    validates_presence_of :content
+    validates_presence_of :user_id
     has_many :comments
 end
